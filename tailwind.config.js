b655630154cb5a2a07c705ef69852c3be50bbf6b
fbelo <fran.m.belo@gmail.com>
2024-01-18 00:41:55 +0000

    tailwind working

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.vue',
+    './src/**/*.jsx',
+    './src/**/*.js',
+    './index.html',
+    './src/**/*.tsx',
+    './src/**/*.ts',     
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
