ec0e96675c52674d634a846d5236594ec7ec8bed
Francisco Belo <fran.m.belo@gmail.com>
2024-09-21 16:40:27 +0200

    spotify thingy bigger on mobile

--- src/components/MainSection.vue
+++ src/components/MainSection.vue
@@ -3,7 +3,7 @@
         <div class="text-center w-full mt-20 flex flex-col justify-center align-middle items-center">
             <p class="text-sm">Hello there! This gray page will have something.</p>
             <p class="text-sm">For now you can listen to my reggaetton playlist:</p>
-            <div class="mt-20 w-1/3">
+            <div class="mt-20 mx-10 md:w-1/3">
                 <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/4nPfcOKqe0B8nVtGuEJAII?utm_source=generator" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
             </div>
         </div>
