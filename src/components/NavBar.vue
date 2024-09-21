cad0f3da87c7fa4e0d65cb8b43272e6df8b322d7
Francisco Belo <fran.m.belo@gmail.com>
2024-09-21 15:04:19 +0200

    dist to dev

--- src/components/NavBar.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <header class="fixed w-full text-center text-xs flex flex-row bg-white/25 h-20 items-center justify-center">  
-      <div class="wrapper">
-        <p class="text-xs text-black font-bold mb-5">CV in progress for real on god no cap</p>
-      </div>
-    </header>
-</template>
