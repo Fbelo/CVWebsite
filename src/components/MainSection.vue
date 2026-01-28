19b7d46fcf09359847e311e338d05b58d676e765
Francisco Belo <fran.m.belo@gmail.com>
2026-01-28 12:19:53 +0000

    fixed reggaeton

--- src/components/MainSection.vue
+++ src/components/MainSection.vue
@@ -10,7 +10,7 @@ import AboutMe from './AboutMe.vue'
     <div class="h-screen flex flex-col pt-16">
         <div class="text-center w-full mt-20 flex flex-col justify-center align-middle items-center text-xl md:text-4xl px-4">
             <p>I'm currently busy making this extremely complex piece of tech.</p>
-            <p>For now you can listen to my reggaetton playlist:</p>
+            <p>For now you can listen to my reggaeton playlist:</p>
             <div class="mt-20 mx-10 md:w-1/3">
                 <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/4nPfcOKqe0B8nVtGuEJAII?utm_source=generator" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
             </div>
